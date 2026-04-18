c1c25247a5545a99246a1a631ee3ffd2bed7f915
smallchungus <wchen1396@gmail.com>
2026-04-18 09:42:02 -0400

    feat(worker): wire fetch/render/upload handlers based on --stage flag

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"flag"
 	"log/slog"
 	"os"
@@ -11,7 +12,10 @@ import (
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	goredis "github.com/redis/go-redis/v9"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 
+	"github.com/smallchungus/disttaskqueue/internal/handler"
 	"github.com/smallchungus/disttaskqueue/internal/queue"
 	"github.com/smallchungus/disttaskqueue/internal/store"
 	"github.com/smallchungus/disttaskqueue/internal/worker"
@@ -55,12 +59,44 @@ func main() {
 	redis := goredis.NewClient(opts)
 	defer func() { _ = redis.Close() }()
 
+	var h worker.Handler
+	switch *stage {
+	case "test":
+		h = worker.NoopHandler{}
+	case "fetch":
+		cfg, key := loadGoogleCfgAndKey()
+		h = handler.NewFetchHandler(handler.FetchConfig{
+			Store:         store.New(pool),
+			EncryptionKey: key,
+			OAuth2:        cfg,
+			DataDir:       envOr("DATA_DIR", "/data"),
+		})
+	case "render":
+		h = handler.NewRenderHandler(handler.RenderConfig{
+			DataDir:     envOr("DATA_DIR", "/data"),
+			PDFEndpoint: envOr("GOTENBERG_URL", "http://gotenberg:3000"),
+		})
+	case "upload":
+		cfg, key := loadGoogleCfgAndKey()
+		h = handler.NewUploadHandler(handler.UploadConfig{
+			Store:         store.New(pool),
+			EncryptionKey: key,
+			OAuth2:        cfg,
+			Redis:         redis,
+			DataDir:       envOr("DATA_DIR", "/data"),
+			RootFolderID:  envOr("DRIVE_ROOT_FOLDER_ID", ""),
+		})
+	default:
+		slog.Error("unknown stage", "stage", *stage)
+		os.Exit(2)
+	}
+
 	w := worker.New(worker.Config{
 		Stage:             *stage,
 		WorkerID:          worker.NewWorkerID(),
 		Store:             store.New(pool),
 		Queue:             queue.New(redis),
-		Handler:           worker.NoopHandler{},
+		Handler:           h,
 		PopTimeout:        30 * time.Second,
 		HeartbeatTTL:      15 * time.Second,
 		HeartbeatInterval: 5 * time.Second,
@@ -72,6 +108,36 @@ func main() {
 		os.Exit(1)
 	}
 	slog.Info("worker stopped")
+}
+
+func loadGoogleCfgAndKey() (*oauth2.Config, []byte) {
+	clientID := mustEnv("GOOGLE_OAUTH_CLIENT_ID")
+	clientSecret := mustEnv("GOOGLE_OAUTH_CLIENT_SECRET")
+	keyB64 := mustEnv("TOKEN_ENCRYPTION_KEY")
+	key, err := base64.StdEncoding.DecodeString(keyB64)
+	if err != nil {
+		slog.Error("decode TOKEN_ENCRYPTION_KEY", "err", err)
+		os.Exit(1)
+	}
+	if len(key) != 32 {
+		slog.Error("TOKEN_ENCRYPTION_KEY must decode to 32 bytes", "got", len(key))
+		os.Exit(1)
+	}
+	return &oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Endpoint:     google.Endpoint,
+		Scopes:       []string{"https://www.googleapis.com/auth/gmail.readonly", "https://www.googleapis.com/auth/drive.file"},
+	}, key
+}
+
+func mustEnv(k string) string {
+	v := os.Getenv(k)
+	if v == "" {
+		slog.Error("missing env var", "key", k)
+		os.Exit(1)
+	}
+	return v
 }
 
 func envOr(k, def string) string {
